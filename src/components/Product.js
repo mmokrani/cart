87eec5d3d9a7f27248f5ae3545341baac20806f8
Mokrani Mehdi <mokrani.mehdi@gmail.com>
2018-09-26 10:33:46 +0200

    Supprimer le repertoire SRC à la racine

--- src/components/Product.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Product = (product) => {
-    console.log(product);
-    return (<div>
-        <div>Réference : {product.product.label}</div>
-    </div>)
-    };
